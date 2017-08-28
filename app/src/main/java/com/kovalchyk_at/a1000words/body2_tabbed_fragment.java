d2da25858e407a86107dc48b8343bfbc6fea1be2
Kovalchyk_AT <Kovalchyk_AT@i.ua>
2017-08-28 10:43:10 +0300

    старт роботиз другою активністю

---
+++ app/src/main/java/com/kovalchyk_at/a1000words/body2_tabbed_fragment.java
@@ -0,0 +1,13 @@
+package com.kovalchyk_at.a1000words;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class body2_tabbed_fragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.body2_cardview);
+    }
+}
